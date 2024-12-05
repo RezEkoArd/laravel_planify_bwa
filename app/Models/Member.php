32b279e8e91e191844cecfca0cc134ad4444de0e
Rezekoard <rezkyardianto1222@gmail.com>
2024-12-05 15:46:15 +0700

    ""

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Member extends Model
 {
@@ -11,6 +13,17 @@ class Member extends Model
 
     protected $guarded = [];
 
-    
+    protected $with = ['user'];
+
+    // relasi user
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function memberable(): MorphTo 
+    {
+        return $this->morphTo();
+    }
 
 }
